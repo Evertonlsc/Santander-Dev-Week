b1757805499e73035da3969afff43e9764eaa1ad
miriam nakamura <evertonluizcruzs@gmail.com>
2021-05-27 00:52:46 -0300

    Princiais Layouts para criação de tela

--- app/src/main/java/br/com/santanderdevweek/MainActivity.kt
+++ app/src/main/java/br/com/santanderdevweek/MainActivity.kt
@@ -2,11 +2,40 @@ package br.com.santanderdevweek
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 
+@Suppress("UNREACHABLE_CODE")
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val inflate = menuInflater
+        inflate.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item_1 -> {
+                Log.d("CLICK", "Clik no item 1")
+                true
+            }
+            R.id.item_2 -> {
+                Log.d("CLICK", "Clik no item 2")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+
+
+
 
 
     }
